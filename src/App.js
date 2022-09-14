676393ca3539bd5872f6e404205b81330f738c17
MartinVladkov <martin_vladkov@abv.bg>
2022-09-14 18:16:40 +0300

    Main functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://www.omdbapi.com/?apikey=cc4a1a52';
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const respone = await fetch(`${API_URL}&s=${title}`);
+        const data = await respone.json();
+
+       setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
+    return (
+        <div className='app'>
+            <h1>MovieLibrary</h1>
+            <div className='search'>
+                <input placeholder='Search...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}></input>
+                <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchTerm)}></img>
+            </div>
+            {movies?.length > 0 ? (<div className='container'>
+                {movies.map((movie) => (
+                    <MovieCard movie = {movie}/>
+                ))}
+                <MovieCard movie = {movies[0]}/>
+            </div>) : 
+            (<div className='empty'>No movies found</div>)}
+            
+        </div>
+    );
 }
 
 export default App;
